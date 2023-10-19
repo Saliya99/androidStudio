a60b102e4e83bb0bd45449e479c94f4b6cd43260
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-10-19 11:44:48 +0530

    DBHandler.java
    extend the DBHandler class from SQLiteOpenHelper class.

---
+++ SQLite/app/src/main/java/com/example/sqlite/DBHandler.java
@@ -0,0 +1,35 @@
+package com.example.sqlite;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHandler extends SQLiteOpenHelper {
+
+    // Database name and version
+    private static final String DATABASE_NAME = "employee_details";
+    private static final int DATABASE_VERSION = 1;
+
+    // Constructor
+    public DBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        // Create your database tables here
+        String sql = "CREATE TABLE employee(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT,dep TEXT, salary TEXT);";
+        // You can use the `db.execSQL()` method to execute SQL statements
+        sqLiteDatabase.execSQL(sql);
+        // for table creation.
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // Upgrade your database if the version changes
+        // Typically, you would drop existing tables and recreate them
+        String sql = "DROP TABLE IF EXISTS employee_details;";
+        sqLiteDatabase.execSQL(sql);
+        onCreate(sqLiteDatabase);
+    }
+}
