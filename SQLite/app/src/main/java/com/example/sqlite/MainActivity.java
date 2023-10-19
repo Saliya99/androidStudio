1542ecc9eb8cca5dbb6e5d6bbc5010bad6f9a513
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-10-19 12:45:31 +0530

    MainActivity.java

--- SQLite/app/src/main/java/com/example/sqlite/MainActivity.java
+++ SQLite/app/src/main/java/com/example/sqlite/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button insert;
+    Button update;
+    Button delete;
+    Button view;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        insert = (Button) findViewById(R.id.insertBtn);
+        update = (Button) findViewById(R.id.updateBtn);
+        delete = (Button) findViewById(R.id.deleteBtn);
+        view = (Button) findViewById(R.id.viewBtn);
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),AddActivity.class));
+            }
+        });
     }
 }
